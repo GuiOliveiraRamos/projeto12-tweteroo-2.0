d9a4e3b92088b7022bc40046c6dd3a92c64bed19
GuiOliveiraRamos <guioliveiraramos30@gmail>
2023-06-21 09:52:57 -0300

    get implementado

--- src/app.js
+++ src/app.js
@@ -4,5 +4,14 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 
+let users = [];
+let tweets = [];
+
+app.get("/tweets", (req, res) => {
+  if (tweets.lenght === 0) {
+    res.send([]);
+  }
+});
+
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
