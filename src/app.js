9257699f0d324e15ef75aad95ba0abc809ca8693
GuiOliveiraRamos <guioliveiraramos30@gmail>
2023-06-23 09:43:13 -0300

    post implementado

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 let users = [];
 let tweets = [];
@@ -15,9 +16,31 @@ app.get("/tweets", (req, res) => {
   const showTweets = limitarTweets.map((t) => ({
     username: t.username,
     tweet: t.tweet,
+    avatar: usuario.find((u) => u.username === t.username).avatar,
   }));
 
   res.send(showTweets);
+});
+
+app.post("/sign-up", (req, res) => {
+  const { username, avatar } = req.body;
+
+  if (!username || !avatar) {
+    return res.sendStatus(422);
+  }
+
+  users.push({ username, avatar });
+  res.send("Usuário cadastrado!");
+});
+
+app.post("/tweets", (req, res) => {
+  const { username, tweet } = req.body;
+  const verificarUsuario = usuario.find((u) => u.username === username);
+  if (!verificarUsuario) {
+    res.send("UNAUTHORIZED");
+  }
+  tweets.push({ username, tweet });
+  res.send("OK");
 });
 
 const PORT = 5000;
